468071aeee03c92cad12aa90d704ce0e2abd42e5
Abhi Kumar <abhi.kumar2019@vitstudent.ac.in>
2022-06-02 13:12:58 +0530

    Added drawing screen

--- lib/screens/drawing_screen.dart
+++ lib/screens/drawing_screen.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field, unused_local_variable, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+
+import '../Paint/painter.dart';
 
 class DrawingScreen extends StatefulWidget {
   const DrawingScreen({Key? key}) : super(key: key);
@@ -10,8 +12,29 @@ class DrawingScreen extends StatefulWidget {
 }
 
 class _DrawingScreenState extends State<DrawingScreen> {
+  List<Offset> _points=[];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: GestureDetector(
+          onPanUpdate: (DragUpdateDetails details){
+            setState(() {
+              RenderBox object=context.findRenderObject() as RenderBox;
+              Offset localPosition= object.globalToLocal(details.globalPosition);
+
+              _points=List.from(_points)..add(localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details){
+            _points.add(null as Offset);
+          },
+          child: CustomPaint(
+            painter: Painting(points: _points),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+    );
   }
 }
