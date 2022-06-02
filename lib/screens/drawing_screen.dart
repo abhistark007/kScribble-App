6b21981377e19d8a80f498cc834000c9a361b9ad
Abhi Kumar <abhi.kumar2019@vitstudent.ac.in>
2022-06-02 12:48:58 +0530

    initial Commit

---
+++ lib/screens/drawing_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DrawingScreen> createState() => _DrawingScreenState();
+}
+
+class _DrawingScreenState extends State<DrawingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
