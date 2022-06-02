9483094cc92580653f6b15b6f6dcccd2ee7c571e
Abhi Kumar <abhi.kumar2019@vitstudent.ac.in>
2022-06-02 12:56:45 +0530

    Added CustomPaint Feature

---
+++ lib/Paint/painter.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class Painting extends CustomPainter{
+  final List<Offset> points;
+  Painting({
+    required this.points
+  });
+
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint =Paint()
+    ..color=Colors.black
+    ..strokeCap=StrokeCap.round
+    ..strokeWidth=5.0;
+
+
+    for(int i=0;i<points.length-1;i++){
+      // ignore: unnecessary_null_comparison
+      if(points[i]!=null && points[i+1]!=null){
+        canvas.drawLine(points[i], points[i+1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Painting oldDelegate)=> oldDelegate.points!=points;
+
+}
